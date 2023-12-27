f052f02b234b859d7512d8bc4d047f70fae3ffc6
sikkkkkw <gksmsla92@naver.com>
2023-12-27 16:15:52 +0900

    first commit

--- toDoList.js
+++ toDoList.js
@@ -1,57 +1,59 @@
-const toDoForm = document.querySelector("#todo-form")
-const toDoInput = document.querySelector("#todo-form input")
-const toDoList = document.querySelector("#todo-list")
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
 
-let toDos = [ ]
+let toDos = [];
 
 function saveToDos() {
-    // console.log(toDos)
-    
-    localStorage.setItem("todos", JSON.stringify(toDos))
+  // console.log(toDos)
+
+  localStorage.setItem("todos", JSON.stringify(toDos));
 }
 
 function deleteToDo(e) {
-    const li = e.target.parentElement
-    li.remove();
-    toDos = toDos.filter((item) => item.id !== parseInt(li.id))
-    saveToDos()
+  const li = e.target.parentElement;
+  li.remove();
+  toDos = toDos.filter(
+    (item) => item.text !== li.querySelector("span").innerText
+  );
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
-    const li = document.createElement("li");
-    li.setAttribute("id", newTodo.id);
-    li.setAttribute("draggable", "true");
-    const span = document.createElement("span")
-    const button = document.createElement("button")
-    button.innerText = "❌"
-    button.addEventListener("click", deleteToDo)
-    span.innerText = newTodo.text;
-    li.appendChild(span)
-    li.appendChild(button)
-    toDoList.appendChild(li)
+  const li = document.createElement("li");
+  li.setAttribute("id", newTodo.id);
+  li.setAttribute("draggable", "true");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  span.innerText = newTodo.text;
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
 
-function handleToDoSubmit (event) {
-    event.preventDefault();
-    // console.log(toDoInput.value)
-    const newTodoObj = {
-        text: toDoInput.value, 
-        id: Date.now()
-    }
-    toDoInput.value = "";
-    // 그려주는 함수
-    paintToDo(newTodoObj);
-    // 로컬스토리지에 저장
-    toDos.push(newTodoObj)
-    saveToDos();
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  // console.log(toDoInput.value)
+  const newTodoObj = {
+    text: toDoInput.value,
+    id: Date.now(),
+  };
+  toDoInput.value = "";
+  // 그려주는 함수
+  paintToDo(newTodoObj);
+  // 로컬스토리지에 저장
+  toDos.push(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-const savedToDos = localStorage.getItem("todos")
+const savedToDos = localStorage.getItem("todos");
 
-if(savedToDos !== null){
-    const parsedToDos = JSON.parse(savedToDos)
-    toDos = parsedToDos
-    toDos.forEach((item) => paintToDo(item))
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  toDos.forEach((item) => paintToDo(item));
 }
